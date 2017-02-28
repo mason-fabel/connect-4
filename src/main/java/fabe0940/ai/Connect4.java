3151033a214a8528451fa1b40357148c5febea06
Mason Fabel <fabe0940@vandals.uidaho.edu>
2017-02-28 00:29:27 -0800

    Added project structure

---
+++ src/main/java/fabe0940/ai/Connect4.java
@@ -0,0 +1,14 @@
+package fabe0940.ai;
+
+public class Connect4 {
+	public static void main(String[] args) {
+		int i;
+
+		i = 0;
+		for (String s : args) System.out.format("%i: %s\n", i++, s);
+
+		System.out.println("Hello, world!");
+
+		return;
+	}
+}
